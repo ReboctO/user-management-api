abc6fef82efb22fef7050c0409ff40b64ed3cb2a
unknown <MM546@ccs.local>
2025-03-21 18:15:16 +0800

    Yabut

---
+++ src/models/users.model.ts
@@ -0,0 +1,30 @@
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "../config/db";
+import { IUser } from "../interfaces/user.interface";
+
+class User extends Model<IUser> {}
+
+User.init(
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+    timestamps: true,
+  }
+);
+
+export default User;
