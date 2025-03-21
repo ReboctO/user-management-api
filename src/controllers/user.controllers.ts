57bfdee4b4dd1441b7b2a934a66338deb34df99c
erllocaylocay <117888162+erllocaylocay@users.noreply.github.com>
2025-03-21 18:28:00 +0800

    addedcontroller

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import User from "../models/user.model";
+import User from "../models/users.model";
 
 // Get all users
 export const getUsers = async (req: Request, res: Response) => {
