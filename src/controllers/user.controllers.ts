f4f436de085834fc5d196c3d05d1e5c44de917cd
unknown <MM546@ccs.local>
2025-03-24 19:09:04 +0800

    A

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -6,6 +6,7 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
   const users = await User.findAll();
   res.json(users); // ✅ No return here
 };
+
 // Get user by ID
 export const getUserById = async (
   req: Request,
@@ -18,7 +19,30 @@ export const getUserById = async (
   }
   res.json(user);
 };
- 
+
+export const createUser = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { name, email, age, password } = req.body;
+  try {
+    // ✅ Check if email already exists
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+      res.status(400).json({ message: "Email already registered" });
+      return;
+    }
+
+    const user = await User.create({ name, email, age, password });
+    res.status(201).json(user);
+    return;
+  } catch (error) {
+    console.error("❌ Error creating user:", error);
+    res.status(400).json({ message: "Error creating user", error });
+    return;
+  }
+};
+
 // Update user by ID
 export const updateUser = async (
   req: Request,
