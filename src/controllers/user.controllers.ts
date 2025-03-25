4b501a1a58a0694d67e736afa30cf28996b096fb
Joseph Arnel Yabut <yabutjosepharnel@gmail.com>
2025-03-26 01:46:04 +1200

    Complete

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import User from "../models/users.model"; // Correct path to your model
+
 // Get all users
 export const getUsers = async (req: Request, res: Response): Promise<void> => {
   const users = await User.findAll();
@@ -19,6 +20,29 @@ export const getUserById = async (
   res.json(user);
 };
 
+export const createUser = async (
+    req: Request,
+    res: Response
+  ): Promise<void> => {
+    const { name, email, age, password } = req.body;
+    try {
+      // ✅ Check if email already exists
+      const existingUser = await User.findOne({ where: { email } });
+      if (existingUser) {
+        res.status(400).json({ message: "Email already registered" });
+        return;
+      }
+  
+      const user = await User.create({ name, email, age, password });
+      res.status(201).json(user);
+      return;
+    } catch (error) {
+      console.error("❌ Error creating user:", error);
+      res.status(400).json({ message: "Error creating user", error });
+      return;
+    }
+  };
+  
 
 // Update user by ID
 export const updateUser = async (
